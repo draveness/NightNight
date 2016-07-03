5d4c10952b387152bb4387413decab77e3dadc58
Draveness <stark.draven@gmail.com>
2016-07-03 16:22:57 +0800

    Add UIColor convience method with alpha param

--- NightNight/Classes/MixedColor.swift
+++ NightNight/Classes/MixedColor.swift
@@ -33,10 +33,14 @@ public class MixedColor {
 
 extension UIColor {
     convenience init(rgb: Int) {
+        self.init(rgb: rgb, alpha: 1.0)
+    }
+
+    convenience init(rgb: Int, alpha: CGFloat) {
         let red = CGFloat(rgb >> 16) / 255.0
         let green = CGFloat(rgb >> 8 & 0xff) / 255.0
         let blue = CGFloat(rgb >> 0 & 0xff) / 255.0
 
-        self.init(red: red, green: green, blue: blue, alpha: 1.0)
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
     }
 }
