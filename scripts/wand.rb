5323a52ddbc2bc5a1dfc3e8514906664bee5acf9
Draveness <stark.draven@gmail.com>
2016-07-06 17:37:50 +0800

    Compelete ruby wand to gen swift file

---
+++ scripts/wand.rb
@@ -0,0 +1,41 @@
+require 'json'
+require 'fileutils'
+require 'erb'
+require 'ostruct'
+
+class ObjcClass
+
+    attr_accessor :properties
+    attr_accessor :name
+    def initialize(name, properties = [])
+        @name = name
+        @properties = properties
+    end
+    
+end
+
+json = JSON.parse File.read('property.json')
+# json.map do |kls, properties|
+#     ObjcClass.new(kls, properties)
+# end
+
+
+class ErbalT < OpenStruct
+    def self.render_from_hash(t, h)
+        ErbalT.new(h).render(t)
+    end
+
+    def render(template)
+        ERB.new(template).result(binding)
+    end
+end
+
+def render(template, hash)
+    erb = File.open(template).read
+    ErbalT::render_from_hash(erb, hash)
+end
+
+json.each do |kls, properties|
+    file_name = "#{kls}+Mixed.swift"
+    File.write file_name, render("extension.swift.erb", { :kls => kls, :properties => properties })
+end
