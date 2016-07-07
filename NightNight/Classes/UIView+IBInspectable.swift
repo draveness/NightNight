b4ec339d8654ce9fbdc79b5c09ca055cc52b34ba
Draveness <stark.draven@gmail.com>
2016-07-07 18:02:19 +0800

    Add IBInspectable group to project

---
+++ NightNight/Classes/UIView+IBInspectable.swift
@@ -0,0 +1,18 @@
+//
+//  UIView+IBInspectable.swift
+//  Pods
+//
+//  Created by Draveness on 7/7/16.
+//
+//
+
+import Foundation
+
+public extension UIView {
+    @IBInspectable public var nightBackgroundColor: UIColor? {
+        get { return objc_getAssociatedObject(self, &NightKeys.backgroundColor) as? UIColor }
+        set {
+            objc_setAssociatedObject(self, &NightKeys.backgroundColor, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+}
