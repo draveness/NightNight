9f5627f793fb0a0995a3b44d399654ee08e8c9f2
Draveness <stark.draven@gmail.com>
2016-07-11 13:39:45 +0800

    Fixed observe noti with _updateTheme method

--- NightNight/Classes/NSObject+Mixed.swift
+++ NightNight/Classes/NSObject+Mixed.swift
@@ -8,7 +8,18 @@
 
 import Foundation
 
+private var isRegisteredNotificationKey = "isRegisteredNotificationKey"
+
 extension NSObject {
+
+    private var isRegisteredNotification: Bool {
+        get {
+            return (objc_getAssociatedObject(self, &isRegisteredNotificationKey) as? NSNumber)?.boolValue ?? false
+        }
+        set {
+            objc_setAssociatedObject(self, &isRegisteredNotificationKey, NSNumber(bool: newValue), .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
     
     func getMixedColor(key: UnsafePointer<Void>) -> MixedColor? {
         return objc_getAssociatedObject(self, key) as? MixedColor
@@ -16,7 +27,10 @@ extension NSObject {
     func setMixedColor(key: UnsafePointer<Void>, value: MixedColor?) {
         objc_setAssociatedObject(self, key, value, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
 
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(_updateTheme), name: NightNightThemeChangeNotification, object: nil)
+        if !isRegisteredNotification {
+            NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(_updateTheme), name: NightNightThemeChangeNotification, object: nil)
+            isRegisteredNotification = true
+        }
     }
 
     func _updateTheme() {
